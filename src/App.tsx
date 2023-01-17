3affc6159a9f0a22c3d8072d0b0797bf56b38dff
noobiecoderhere <rkrishna1997@gmail.com>
2023-01-17 20:46:50 +0530

    Initial ESLint and prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+function App(): JSX.Element {
+  return <div className='App'>Hello</div>;
 }
 
 export default App;
